dffbdd402caadafd411126efeb0d855d07800667
160795-yu <fjuais08@fju.edu.tw>
2025-12-29 13:41:18 +0800

    [feat] update base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/lotteryVueApp/', // GitHub repository name
+  base: '/lottery/', // GitHub repository name
   plugins: [
     vue(),
     vueDevTools(),
